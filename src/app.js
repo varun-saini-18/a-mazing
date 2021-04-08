c3d93b1a86f2d82da535c1eb22e880f50272c744
varun <varunnssaini@gmail.com>
2021-04-08 19:26:11 +0530

    Initial Phase of app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
+
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer.js';
+
+const jsx = ( 
+  <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'))
